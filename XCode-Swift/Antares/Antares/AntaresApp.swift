4f780b216f17539f084518ba3a33e609a9a651fe
Matthew Gomoka <mgomoka@bu.edu>
2021-07-17 16:47:36 -0400

    commit

---
+++ XCode-Swift/Antares/Antares/AntaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  AntaresApp.swift
+//  Antares
+//
+//  Created by Matthew Gomoka on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct AntaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
