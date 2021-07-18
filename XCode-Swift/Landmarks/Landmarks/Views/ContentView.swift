8fcd666f0fd4b1974142a70146a56acb687666c6
Matthew Gomoka <mgomoka@bu.edu>
2021-07-17 20:52:04 -0400

    commit

--- XCode-Swift/Landmarks/Landmarks/Views/ContentView.swift
+++ XCode-Swift/Landmarks/Landmarks/Views/ContentView.swift
@@ -9,34 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-                
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                Divider()
-                Text("Turtle Rock is a rock formation in Joshua Tree National Park, California.")
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
---
+++ XCode-Swift/Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Matthew Gomoka on 7/17/21.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+                
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("Turtle Rock is a rock formation in Joshua Tree National Park, California.")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
