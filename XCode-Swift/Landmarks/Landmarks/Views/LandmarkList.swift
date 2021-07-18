fc9236e453d7fee0e6a83465513561073cbf6d71
Matthew Gomoka <mgomoka@bu.edu>
2021-07-17 20:21:33 -0400

    commit

---
+++ XCode-Swift/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Matthew Gomoka on 7/17/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+            
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
